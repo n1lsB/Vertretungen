6418dd2deb0914fd58a2765fb6633dc419f4d267
n1lsb <n.beyer@outlook.com>
2018-05-07 16:27:20 +0200

    Change misleading text and perform code optimization
    Signed-off-by: n1lsb <n.beyer@outlook.com>

--- app/src/main/java/de/nils_beyer/android/Vertretungen/mainActivity/viewholder/EventViewHolder.kt
+++ app/src/main/java/de/nils_beyer/android/Vertretungen/mainActivity/viewholder/EventViewHolder.kt
@@ -6,17 +6,16 @@ import android.view.View
 import android.widget.TextView
 import de.nils_beyer.android.Vertretungen.R
 import de.nils_beyer.android.Vertretungen.events.Event
-import kotlinx.android.synthetic.main.recyclerview_overview_event.view.*
 import java.text.SimpleDateFormat
 import java.util.*
 
 class EventViewHolder(val v: View, val context : Context) : RecyclerView.ViewHolder(v) {
-    val textTitle = v.findViewById<TextView>(R.id.text_event_title)
-    val textTime = v.findViewById<TextView>(R.id.text_event_time)
+    private val textTitle = v.findViewById<TextView>(R.id.text_event_title)
+    private val textTime = v.findViewById<TextView>(R.id.text_event_time)
 
 
     internal fun bind(event : Event) {
-        textTitle.text = event.title;
+        textTitle.text = event.title
 
         val dateFormat = SimpleDateFormat("dd.MM", Locale.GERMANY)
         var timeString = dateFormat.format(event.start)
